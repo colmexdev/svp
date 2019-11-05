b9663facb53c41f65f303ce95328dc02fab4e643
colmexdev <github@colmex.mx>
2019-11-05 16:15:51 -0600

    Multimedia

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,5 +1,8 @@
 document.addEventListener("resize", function(){
 	document.querySelector("#body-wrap").style.paddingBottom = document.querySelector("#footer").offsetHeight + "px";
+	try {
+		document.querySelector("#slider").style.height = (document.querySelector("#slider").offsetWidth * 11 / 20) + "px";
+	} catch(e) { }
 });
 
 window.addEventListener("load", function(){
