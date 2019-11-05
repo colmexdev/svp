710c40c8f9c2fbe183a980de485f41de72ced041
colmexdev <github@colmex.mx>
2019-11-05 16:35:50 -0600

    Multimedia

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,4 +1,4 @@
-document.addEventListener("resize", function(){
+window.addEventListener("resize", function(){
 	document.querySelector("#body-wrap").style.paddingBottom = document.querySelector("#footer").offsetHeight + "px";
 	try {
 		document.querySelector("#slider").style.height = (window.outerWidth * 11 / 20) + "px";
@@ -6,5 +6,5 @@ document.addEventListener("resize", function(){
 });
 
 window.addEventListener("load", function(){
-	document.dispatchEvent(new Event("resize"));
+	window.dispatchEvent(new Event("resize"));
 });
