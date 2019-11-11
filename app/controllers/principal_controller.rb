465d2871a86613b3e490e6037a14909d65ccdaee
colmexdev <github@colmex.mx>
2019-11-11 15:38:03 -0600

    Sesiones

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -14,6 +14,7 @@ class PrincipalController < ApplicationController
 
   def seminarios
     @title = "Seminarios"
+    @sems = Sesion.where("date_part('year', fecha_i) = ?", (params[:anio] ? params[:anio] : Sesion.maximum(:fecha_i).year.to_s))
     respond_to do |format|
       format.html
       format.js
