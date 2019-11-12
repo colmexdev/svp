8ef63c1918a608b97ff839da6caafbb34c5c763f
colmexdev <github@colmex.mx>
2019-11-12 12:45:00 -0600

    Sesiones

--- app/models/sesion.rb
+++ app/models/sesion.rb
@@ -1,3 +1,18 @@
 class Sesion < ApplicationRecord
+  before_save :delete_banner, if: -> {banner_del == '1'}
+
+  validates_presence_of :titulo
+  has_attached_file :banner, styles: {},
+      url: '/assets/banners_sesiones/:style/:id/:basename.:extension',
+      path: ':rails_root/public/assets/banners_sesiones/:style/:id/:basename.:extension',
+      default_url: '/vacio.png'
+
+  validates_attachment_content_type :banner, content_type: ["image/jpg", "image/jpeg", "image/png"]
+
+  protected
+
+  def delte_banner
+    self.banner = nil
+  end
 
 end
