485de56155d1724860bc7da5c0a9546dbcca2e65
colmexdev <github@colmex.mx>
2020-01-29 16:10:24 -0600

    Banner

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -1,7 +1,7 @@
 window.addEventListener("resize", function(){
 	document.querySelector("#body-wrap").style.paddingBottom = document.querySelector("#footer").offsetHeight + "px";
 	try {
-		document.querySelector("#slider").style.height = (window.outerWidth * 11 / 20) + "px";
+		document.querySelector("#slider").style.height = (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * 11 / 20) + "px";
 	} catch(e) { }
 });
 
