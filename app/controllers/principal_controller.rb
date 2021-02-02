6b937aa4f25754b4d460de986cf786e8b51602fc
colmexdev <github@colmex.mx>
2021-02-02 13:33:29 -0600

    Seminario

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -32,14 +32,14 @@ class PrincipalController < ApplicationController
     @title = @sem.titulo
     @banner = @sem.banner.url if @sem.banner
     if @sem.banner.nil?
-      begin
+      #begin
         @cliente = TinyTds::Client.new username: ENV["AG_USR"], password: ENV["AG_PWD"], host: ENV["AG_HOST"], port: ENV["AG_PORT"], database: ENV["AG_DB"]
         @resultado = @cliente.execute("SELECT ligaImagen FROM dbo.vw_DatosAgenda WHERE tituloActividad = '#{@sem.titulo}'")
         @banner = @resultado.first['ligaImagen']
-      rescue
-        @resultado = []
-        @cliente.close if @cliente
-      end
+      #rescue
+      #  @resultado = []
+      #  @cliente.close if @cliente
+      #end
     end
   end
 
